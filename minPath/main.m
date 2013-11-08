bc6a35d652dce2a476f03f23b01e4e55d625c635
Jifu Zhou <zhoustoic@gmail.com>
2013-11-08 12:35:08 -0500

    updated script with comments

--- minPath/main.m
+++ minPath/main.m
@@ -1,12 +1,16 @@
 function main
+
+%testing matrix M
 global M
 M=[0 0 1 1 2 3 4;
    1 0 1 2 4 5 3;
    3 3 10 3 4 2 6;
    5 6 7 30 0 1 1];
 
+%find path going rightward/downward and with min val 
 path=minPath3(M);
 path
+
 end
 
 %recursive solution
@@ -18,7 +22,7 @@ m=size(M,2);
 
 global targetLen
 targetLen=min(n,m);
-% sets=unique(M);
+
 a=1;b=1;
 Q=getQ(M,a,b);
 [path,val]=minPath3R(M,a,b,0,Q);
@@ -28,6 +32,7 @@ val
 
 end
 
+%recursively get path with min val, in M(a:end,b:end) starting (a,b);
 function [path,val]=minPath3R(M,a,b,len,Q)
 
 
@@ -58,17 +63,6 @@ for i=1:size(Q,1)
 
     Qtemp=Q;
     Qtemp(i,:)=[];
-% %     Qtemp(Qtemp(:,1)<x,:)=[];
-% %     Qtemp(Qtemp(:,2)<y,:)=[];
-%     Qtemp(:,1)=Qtemp(:,1)-(x-1);
-%     Qtemp(:,2)=Qtemp(:,2)-(y-1);
-%     Qtemp(Qtemp(:,1)<-1,:)=[];
-%     Qtemp(Qtemp(:,2)<-1,:)=[];
-%     x
-%     y
-%     Qtemp
-%     M
-
     
     [path1, val]=minPath3R(M,x,y,len+1,Qtemp);
     path1s{end+1}=path1;
@@ -83,11 +77,12 @@ val=v;
 
 end
 
+%get queue of values to explore (sorted)
 function Q=getQ(M,a0,b0)
 
     sets=unique(M);
     [a,b]=find(M==sets(1));
-    %remove i < a0,b0
+    %remove indices where < a0,b0
     ia=a<a0;
     ib=b<b0;
     I=ia | ib;
@@ -108,6 +103,7 @@ function Q=getQ(M,a0,b0)
     end
 end
 
+%sort indices (a,b) by rank - defined as the area(M)-area(square ahead)
 function [a,b]=sortByRank(M,ab)
     n=size(M,1);
     m=size(M,2);
@@ -127,19 +123,19 @@ function [a,b]=sortByRank(M,ab)
     end
 end
 
-function rank=getRank(M,x,y)
-    n=size(M,1);
-    m=size(M,2);
-    rank=n*m-(n-x)*(m-y);
-end
+% function rank=getRank(M,x,y)
+%     n=size(M,1);
+%     m=size(M,2);
+%     rank=n*m-(n-x)*(m-y);
+% end
 
 
 
 
 
 
-%fast but naive solution - strong assumption of matrix M: increase
-%diagnoally
+%fast but naive solution - strong assumption of matrix M: values increase
+%diagnoally (top-left to bottom-right)
 function path=minPath2(M)
 
 n=size(M,1);
@@ -159,35 +155,32 @@ for i=1:length(sets)
     if count+length(a)>=pathLen
         num=pathLen-count;  
         
-        start=1;
-        
-        append=[a(start:start+num-1) b(start:start+num-1) ones(num,1)*sets(i)];
-        
-        a0=path(:,1);
-        b0=path(:,2);
-        
-        for ii=1:length(a0)
-            a0b0(end+1)=n*m-(n-a0(ii))*(m-b0(ii));
-        end
-        for ii=1:length(a)
-            ab(end+1)=n*m-(n-a(ii))*(m-b(ii));
-        end
-%         a0b0=a0+b0;
-%         ab=a(start:start+num-1)+b(start:start+num-1);
-        
-%         ranks=getRank([path())
-        [v,I]=sort(path());
-        I=I(:,1);
-        path=[path; append];
-        temp=path;
-        for i=1:length(path)
-            if i>1 && I(i)==I(i-1)
-                i=i+1;
-            end
-            temp(i,:)=path(I(i),:);
-        end
-        path=temp;
-    
+%         start=1;
+%         
+%         append=[a(start:start+num-1) b(start:start+num-1) ones(num,1)*sets(i)];
+%         
+%         a0=path(:,1);
+%         b0=path(:,2);
+%         
+%         for ii=1:length(a0)
+%             a0b0(end+1)=n*m-(n-a0(ii))*(m-b0(ii));
+%         end
+%         for ii=1:length(a)
+%             ab(end+1)=n*m-(n-a(ii))*(m-b(ii));
+%         end
+% 
+%         [v,I]=sort(path());
+%         I=I(:,1);
+%         path=[path; append];
+%         temp=path;
+%         for i=1:length(path)
+%             if i>1 && I(i)==I(i-1)
+%                 i=i+1;
+%             end
+%             temp(i,:)=path(I(i),:);
+%         end
+%         path=temp;
+%     
     
         
         path=addToPath(path,sets(i),a(1:num),b(1:num));
@@ -231,7 +224,10 @@ path=temp;
 end
 
 
-%CMA-ES solution, not garantee global optimum
+
+
+
+%CMA-ES/fmincon solution, not garantee global optimum
 function path= minPath1(M)
 
 n=size(M,1);
@@ -250,7 +246,7 @@ path0=[path0(:,1);path0(:,2)];
 
 opt.StopFitness=(mean(M(:))*min(n,m))-1;
 [path,fmin]=cmaes('fitfun',path0,1,opt);
-fmin
+
 n=length(path);
 path=[path(1:n/2) path(n/2+1:n)];
 path=int8(path)
@@ -258,52 +254,52 @@ path=int8(path)
 end
 
 
-function f=myfun(path)
-global M
-f=0;
-for i=1:length(path)
-    x=int8(path(i,1));
-    y=int8(path(i,2));
-    f=f+M(x,y);
-end
 
-f
-
-
-end
-
-function [c,ceq]=mycon(path)
-global M
-n=size(M,1);
-m=size(M,2);
-
-path=int8(path);
-c=[];
-ceq=[];
-x0=path(1,1);
-y0=path(1,2);
-for i=2:length(path)
-    x1=path(i,1);
-    y1=path(i,2);
-    c(end+1)=x1-x0;%>0
-    c(end+1)=y1-y0;%>0
-    
-    %boundary
-    c(end+1)=x1-1;%>0
-    c(end+1)=y1-1;%>0
-    c(end+1)=x0-1;%>0
-    c(end+1)=y0-1;%>0
-    c(end+1)=n-x1;%>0
-    c(end+1)=m-y1;%>0
-    c(end+1)=n-x0;%>0
-    c(end+1)=m-y0;%>0
-    
-%     ceq(end+1)=x1-x0;%=0
-%     ceq(end+1)=y1-y0;%=0
-    x0=x1;
-    y0=y1;
-end
-
-c
-ceq
-end
+% %Used for fmincon
+% function f=myfun(path)
+% global M
+% f=0;
+% for i=1:length(path)
+%     x=int8(path(i,1));
+%     y=int8(path(i,2));
+%     f=f+M(x,y);
+% end
+% 
+% 
+% end
+% 
+% function [c,ceq]=mycon(path)
+% global M
+% n=size(M,1);
+% m=size(M,2);
+% 
+% path=int8(path);
+% c=[];
+% ceq=[];
+% x0=path(1,1);
+% y0=path(1,2);
+% for i=2:length(path)
+%     x1=path(i,1);
+%     y1=path(i,2);
+%     c(end+1)=x1-x0;%>0
+%     c(end+1)=y1-y0;%>0
+%     
+%     %boundary
+%     c(end+1)=x1-1;%>0
+%     c(end+1)=y1-1;%>0
+%     c(end+1)=x0-1;%>0
+%     c(end+1)=y0-1;%>0
+%     c(end+1)=n-x1;%>0
+%     c(end+1)=m-y1;%>0
+%     c(end+1)=n-x0;%>0
+%     c(end+1)=m-y0;%>0
+%     
+% %     ceq(end+1)=x1-x0;%=0
+% %     ceq(end+1)=y1-y0;%=0
+%     x0=x1;
+%     y0=y1;
+% end
+% 
+% c
+% ceq
+% end
